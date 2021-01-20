1c37b9788ce9d583988a2295f0310054e1f7c823
fengke <fengke2016@qq.com>
2021-01-20 20:32:21 +0800

    Initial commit

---
+++ nacos-provide/src/test/java/com/feker/nacosprovide/NacosProvideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feker.nacosprovide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosProvideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
